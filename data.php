becd4e40a08731e9656f390b6beb77c8924eb7bf
administrator <silva.mkc510@gmail.com>
2022-11-03 15:34:46 +0530

    first commit

---
+++ data.php
@@ -0,0 +1,7 @@
+<?php
+$data = array(
+    'name'=> 'Test 1',
+    'empNumber'=> 'emp1');
+ echo(json_encode($data));
+
+ ?>
---
+++ data/testcasetemp.js
@@ -0,0 +1,5 @@
+var graphInfo = {empNumber: 'emp1',name: 'Test 1', jobTitle: 'Software Engineer', location: 'Los Angeles', subunit: 'Orange Department A', children: [{empNumber: 'emp2',name: 'Test 2', jobTitle: 'Software Engineer', location: 'Los Angeles', subunit: 'Orange Department A', children: []}]}
+
+
+// , children:
+// , jobTitle: 'Software Engineer', location: 'Los Angeles', subunit: 'Orange Department A', children:
